c11f2f9ecf24a7c0c64bf3db48a819b6e5e2615b
Nataliess <n.borisova@me.com>
2025-07-02 13:43:00 +0300

    initial commit

---
+++ src/StaffTypesServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Notabenedev\StaffTypes;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class StaffTypesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
---
+++ src/config/staff-types.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "sitePackageName" => "Типы специалистов",
+    "siteStaffTypeName" => "Тип специалистов",
+    "siteEmployeeOfferName" => "Предложение специалиста",
+
+];
